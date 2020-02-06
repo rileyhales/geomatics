b0781985170fe9c6a2e21440d5c19ed7b5aa6508
rileyhales <rileyhales1@gmail.com>
2020-02-06 16:53:00 -0700

    v0.2 numbering

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme:
 setup(
     name='geomatics',
     packages=['geomatics'],
-    version='0.1',
+    version='0.2',
     description='GIS tools developed by Riley Hales for the BYU Hydroinformatics Lab',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -14,7 +14,6 @@ setup(
     project_urls=dict(Documentation='https://geomatics.readthedocs.io',
                       Source='https://github.com/rileyhales/geomatics'),
     license='BSD 3-Clause',
-    license_family='BSD',
     python_requires='>=3',
     install_requires=['rasterio', 'rasterstats', 'xarray', 'netcdf4', 'python-dateutil', 'numpy', 'pandas']
 )
