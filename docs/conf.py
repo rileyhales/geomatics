61d3bc780fc3b398987ddf7e3523dc968f120109
rileyhales <rileyhales1@gmail.com>
2020-05-15 11:03:42 -0600

    fix docs mock-imports

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,8 @@ autodoc_mock_imports = [
     'shapefile',
     'pyproj',
     'xarray',
-    'h5py'
+    'h5py',
+    'affine'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
